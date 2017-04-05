aad4f1c2159f1d4bb3df88cde8c378c93b6712dc
carecinos <carecinos@cpp.edu>
2017-04-04 23:16:26 -0700

    EchoServer and EchoClient working.

--- CS380-EX1/src/EchoClient.java
+++ CS380-EX1/src/EchoClient.java
@@ -1,7 +1,10 @@
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public final class EchoClient {
 
@@ -11,6 +14,17 @@ public final class EchoClient {
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             System.out.println(br.readLine());
+            Scanner kb = new Scanner(System.in);
+    	    OutputStream os = socket.getOutputStream();
+    	    PrintStream ps = new PrintStream(os);
+    	    System.out.print("Client> ");
+    	    String echo;
+    	    
+    	    while(!(echo = kb.nextLine()).equals("exit")){
+    	    	ps.println(echo);
+    	    	System.out.println("Server> " + br.readLine());
+    	    	System.out.print("Client> ");
+    	    }
         }
     }
 }
--- CS380-EX1/src/EchoServer.java
+++ CS380-EX1/src/EchoServer.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream; 
 import java.io.PrintStream;
 import java.net.ServerSocket;
@@ -14,7 +17,20 @@ public final class EchoServer {
                     OutputStream os = socket.getOutputStream();
                     PrintStream out = new PrintStream(os, true, "UTF-8");
                     out.printf("Hi %s, thanks for connecting!%n", address);
+                    
+                    InputStream is = socket.getInputStream();
+					InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+			       	BufferedReader br = new BufferedReader(isr);
+				    String message;
+				    
+					while((message = br.readLine())!=null){
+						out.println(message);
+				    }
+					
+				    System.out.printf("Client disconnected: %s%n", address);
+					socket.close();
                 }
+                catch (Exception e){}
             }
         }
     }
